99b95e49f2b8104be1293f702fffd51b2ba54f03
Vicky Sony <vigneshwaransdet@gmail.com>
2021-09-15 17:38:02 +0530

    code updated

--- AlgorithmsWiseProblems/src/com/twopointers/easy/SplitAStringInBalancedStrings_1221.java
+++ AlgorithmsWiseProblems/src/com/twopointers/easy/SplitAStringInBalancedStrings_1221.java
@@ -3,6 +3,26 @@ package com.twopointers.easy;
 import org.junit.Assert;
 import org.junit.Test;
 
+/*
+ * Input -> String
+ * Output -> int
+ * Constraints -> O(n)
+ * Pseudocode:
+ * initialize left=0,rightCount=0,leftCount=0,count=0
+ * Iterate the loop until left < s.length()
+ * check if s.charAt(left++)=='R'
+ * 			true increment the rightCount
+ * 		else	
+ * 			increment the leftCount
+ * 		check rightCount == leftCount
+ * 		true increment the count
+ * 
+ * once loop ends 
+ * return count
+ * 
+ * Time Complexity -> O(n)
+ * Space Complexity -> O(1)
+ */
 
 public class SplitAStringInBalancedStrings_1221 {
 	@Test
@@ -30,13 +50,13 @@ public class SplitAStringInBalancedStrings_1221 {
 	}
 
 	private int toSplitAStringInBalancedStrings(String s) {
-		int left=0,rC=0,lC=0,c=0;
+		int left=0,rightCount=0,leftCount=0,count=0;
 		while(left<s.length()) {
-			if(s.charAt(left++)=='R') rC++;
-			else lC++;
-			if(rC==lC) c++;
+			if(s.charAt(left++)=='R') rightCount++;
+			else leftCount++;
+			if(rightCount==leftCount) count++;
 		}
-		return c;
+		return count;
 	}
 
 }
