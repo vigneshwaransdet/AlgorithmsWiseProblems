6b7b9324a4d0f7c9f53f02a441c327f84a1bae76
Vicky Sony <vigneshwaransdet@gmail.com>
2021-09-20 15:02:56 +0530

    Code Added

--- AlgorithmsWiseProblems/src/com/hashing/easy/UniqueNumberOfOccurrences_1207.java
+++ AlgorithmsWiseProblems/src/com/hashing/easy/UniqueNumberOfOccurrences_1207.java
@@ -1,7 +1,7 @@
 package com.hashing.easy;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,19 +11,48 @@ public class UniqueNumberOfOccurrences_1207 {
 		int[] arr = {1,2,2,1,1,3};
 		Assert.assertTrue(toFindUniqueNumberOfOccurrences(arr));
 	}
-
+	
+	@Test
+	public void test2() {
+		int[] arr = {1,2};
+		Assert.assertFalse(toFindUniqueNumberOfOccurrences(arr));
+	}
+	
+	@Test
+	public void test3() {
+		int[] arr = {-3,0,1,-3,1,1,1,-3,10,0};
+		Assert.assertTrue(toFindUniqueNumberOfOccurrences(arr));
+	}
+	
+	@Test
+	public void test4() {
+		int[] arr = {26,2,16,16,5,5,26,2,5,20,20,5,2,20,2,2,20,2,16,20,16,17,16,2,16,20,26,16};
+		Assert.assertFalse(toFindUniqueNumberOfOccurrences(arr));
+	}
 	private boolean toFindUniqueNumberOfOccurrences(int[] arr) {
 		HashMap<Integer,Integer> map = new HashMap<>();
 		for (int i = 0; i < arr.length; i++) {
 			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
 		}
 		System.out.println(map);
-		for (Map.Entry<Integer, Integer> out : map.entrySet()) {
-			int index=1;
-			if(out.getValue()==map.get(arr[index++])) {
-				return false;
-			}
-		}
-		return true;
+//		this is failed for test4() method
+//		int index = 0;
+//		int preValue = 0;
+
+//		for (Map.Entry<Integer, Integer> out : map.entrySet()) {
+//			if(index>0) {
+//				if(preValue == out.getValue()) {
+//					return false;
+//				}
+//			}
+//			preValue = out.getValue();
+//			index++;
+//		}
+		
+		HashSet<Integer> set = new HashSet<>(map.values());
+		System.out.println(set);
+		if(map.size()==set.size()) return true;
+		
+		return false;
 	}
 }
